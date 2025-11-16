59ab8c1af56f1cceac5166bf9b913b5ac3156b42
Radwa492 <radwanasr5174@gmail.com>
2025-11-16 23:21:11 +0200

    update2 task3

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,8 +1,5 @@
-size = int(input("Enter a positive integer for the size of the square pattern: "))
-if size <= 0:
-    print("Please enter a positive integer.")
-else:
-    for i in range(size):
+size = int(input("Enter the size of the pattern: "))
+for i in range(size):
         for j in range(size):
             print("*", end=" ")
         print()
